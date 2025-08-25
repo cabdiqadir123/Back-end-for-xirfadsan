24f26124e04e4e6935549a00615790e57bac2e32
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-08-25 16:53:10 +0300

    backend updated

--- Router/EarningRouter.js
+++ Router/EarningRouter.js
@@ -35,7 +35,7 @@ EarningRouter.post('/add', (req, res) => {
     const { name, booking_id, amount,status } = req.body;
     console.log(req.body);
     mysqlconnection.query(
-        'insert into earnings(user_id,booking_id,amount,status) values((select id from users where name=?),?,?,?);',
+        'insert into earnings(user_id,booking_id,amount,status) values(?,?,?,?);',
         [name, booking_id, amount, status], (error, rows, fields) => {
             if (!error) {
                 res.json({ status: 'inserted' });
