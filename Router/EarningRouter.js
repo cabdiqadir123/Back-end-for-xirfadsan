35448de476529b0253ba0928c764ad677fa88a1a
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-08-25 18:40:39 +0300

    backend updated

--- Router/EarningRouter.js
+++ Router/EarningRouter.js
@@ -31,12 +31,25 @@ EarningRouter.get('/earningstaff', (req, res) => {
     });
 });
 
+// EarningRouter.post('/add', (req, res) => {
+//     const { user_id, booking_id, amount,status } = req.body;
+//     console.log(req.body);
+//     mysqlconnection.query(
+//         'insert into earnings(user_id,booking_id,amount,status) values(?,?,?,?);',
+//         [user_id, booking_id, amount, status], (error, rows, fields) => {
+//             if (!error) {
+//                 res.json({ status: 'inserted' });
+//             } else {
+//                 console.log(error);
+//             }
+//         });
+// });
 EarningRouter.post('/add', (req, res) => {
-    const { user_id, booking_id, amount,status } = req.body;
+    const { user_id, booking_id , amount,status} = req.body;
     console.log(req.body);
     mysqlconnection.query(
-        'insert into earnings(user_id,booking_id,amount,status) values(?,?,?,?);',
-        [user_id, booking_id, amount, status], (error, rows, fields) => {
+        'insert into earnings(user_id,booking_id,amount,status ) values(?,?,?,?);',
+        [user_id, booking_id,amount,status ], (error, rows, fields) => {
             if (!error) {
                 res.json({ status: 'inserted' });
             } else {
