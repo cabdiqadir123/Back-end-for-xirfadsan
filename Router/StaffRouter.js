4b9bad4c8131a7756b04eea70c24a611b50c6ace
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-08-26 14:33:30 +0300

    backend updated

--- Router/StaffRouter.js
+++ Router/StaffRouter.js
@@ -47,11 +47,11 @@ StaffRouter.get("/image/:id", (req, res) => {
 });
 
 StaffRouter.post('/add', (req, res) => {
-    const { name, supplier_id, sub_service_id,available } = req.body;
+    const { name, service_id,available } = req.body;
     console.log(req.body);
     mysqlconnection.query(
-        'insert into staff(user_id,service_id,available) values((select id from users where name=?),(select service_id from services s  where name=?),?);',
-        [name, supplier_id, sub_service_id,available], (error, rows, fields) => {
+        'insert into staff(user_id,service_id,available) values((select id from users where name=?),(select service_id from services where name=?),?);',
+        [name, service_id,available], (error, rows, fields) => {
             if (!error) {
                 res.json({ status: 'inserted' });
             } else {
