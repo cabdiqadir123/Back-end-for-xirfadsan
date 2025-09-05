6c595a069bb16006eed92f1ec0a0106e740201da
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-09-05 06:38:07 +0300

    backend updated

--- dstsbase/database.js
+++ dstsbase/database.js
@@ -1,24 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const mysqlconnection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
-    // host: 'localhost',
-    // user: 'root',
-    // password: '',
-    // database: 'servicemanagementsystem'
+const mysqlconnection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-mysqlconnection.connect(function (error){
-    if(error){
-        console.log(error);
-    } else{
-        console.log('Database is connected');
-    }
-});
-
-module.exports= mysqlconnection;
+module.exports = mysqlconnection;
