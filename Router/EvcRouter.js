16262db6eb42a17c578a13dfa364f07a07a4f866
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-09-10 16:51:41 +0300

    backend updated

--- Router/EvcRouter.js
+++ Router/EvcRouter.js
@@ -43,7 +43,7 @@ PaymentRouter.post('/pay', async (req, res) => {
             headers: { "Content-Type": "application/json" },
         });
 
-        res.json(response.status);
+        res.json(response.data);
 
     } catch (error) {
         console.error("❌ Payment Error:", error.response ? error.response.data : error.message);
