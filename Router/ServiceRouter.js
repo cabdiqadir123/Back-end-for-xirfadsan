081aa800143490e58752bef7b28f5ee6870c2185
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-10-16 23:12:23 +0300

    backend updated

--- Router/ServiceRouter.js
+++ Router/ServiceRouter.js
@@ -11,7 +11,7 @@ ServiceRouter.get('/', (req, res) => {
 });
 
 ServiceRouter.get('/all', (req, res) => {
-  mysqlconnection.query('select service_id,name,image,color,created_at  from services', (error, rows, fields) => {
+  mysqlconnection.query('select service_id,name,image,color,status,created_at  from services', (error, rows, fields) => {
     if (!error) {
       res.json(rows);
     } else {
