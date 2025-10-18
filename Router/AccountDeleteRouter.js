1ad690852190755fd2dbfddbb994c80fd04cf356
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-10-18 21:02:30 +0300

    backend updated

---
+++ Router/AccountDeleteRouter.js
@@ -0,0 +1,124 @@
+const { Router } = require('express')
+
+const AccountDelRouter = Router();
+
+const mysqlconnection = require('../dstsbase/database.js');
+
+AccountDelRouter.get('/', (req, res) => {
+    res.status(200).json('server on port 9000 and database is connected');
+});
+
+AccountDelRouter.get('/all', (req, res) => {
+    mysqlconnection.query('select * from account_delete',
+        (error, rows, fields) => {
+            if (!error) {
+                res.json(rows);
+            } else {
+                console.log(error);
+            }
+        });
+});
+
+AccountDelRouter.post('/add', (req, res) => {
+    const { name, email, phone, subject, message, is_read, replied_at, created_at } = req.body;
+
+    if (!name || !email || !message) {
+        return res.status(400).json({ error: "Missing required fields (name, email, message)" });
+    }
+
+    const query = `
+    INSERT INTO account_delete 
+    (name, email, phone, subject, message, is_read,replied_at, created_at) 
+    VALUES (?, ?, ?, ?, ?, ?, ?,?);
+  `;
+    const values = [name, email, phone, subject, message, is_read, replied_at, created_at];
+
+    mysqlconnection.query(query, values, (error, result) => {
+        if (error) {
+            console.error("Error inserting contact message:", error);
+            return res.status(500).json({ error: "Failed to insert contact message", details: error });
+        }
+
+        // Return the inserted record ID
+        res.status(201).json({
+            message: "Contact message inserted successfully",
+            id: result.insertId,
+        });
+    });
+});
+
+
+AccountDelRouter.put('/update_is_read/:id', (req, res) => {
+    const id = req.params.id;
+    let { is_read } = req.body;
+
+    is_read = (is_read === true || is_read === 'true' || is_read === 1 || is_read === '1') ? 1 : 0;
+
+    // Using SQL's COALESCE + NULLIF to keep old values when no new data is provided
+    const query = `UPDATE contact_messages SET is_read =? WHERE id = ?;`;
+
+    const values = [is_read, id];
+
+    mysqlconnection.query(query, values, (error, result) => {
+        if (error) {
+            console.error("Error updating contact message:", error);
+            return res.status(500).json({
+                status: "error",
+                message: "Error updating the is read",
+                error: err.message,
+                reqBody: req.body,
+            });
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: "Contact message not found" });
+        }
+
+        res.status(200).json({ message: "Contact message updated successfully", id });
+    });
+});
+
+AccountDelRouter.put('/update_replied_at/:id', (req, res) => {
+    const id = req.params.id;
+    let { is_read, replied_at } = req.body;
+
+    is_read = (is_read === true || is_read === 'true' || is_read === 1 || is_read === '1') ? 1 : 0;
+
+    // Using SQL's COALESCE + NULLIF to keep old values when no new data is provided
+    const query = `UPDATE contact_messages SET is_read= ?,replied_at = ? WHERE id = ?;`;
+
+    const values = [is_read, replied_at, id];
+
+    mysqlconnection.query(query, values, (error, result) => {
+        if (error) {
+            console.error("Error updating contact message:", error);
+            return res.status(500).json({
+                status: "error",
+                message: "Error updating the is repliaed",
+                error: err.message,
+                reqBody: req.body,
+            });
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: "Contact message not found" });
+        }
+
+        res.status(200).json({ message: "Contact message updated successfully", id });
+    });
+});
+
+AccountDelRouter.post('/delete', (req, res) => {
+    const { id } = req.body;
+    console.log(req.body);
+    mysqlconnection.query('delete from contact_messages where id=?'
+        , [id], (error, rows, fields) => {
+            if (!error) {
+                res.json(rows);
+            } else {
+                res.json({ status: error });
+            }
+        });
+});
+
+module.exports = AccountDelRouter;
--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use('/api/sms/', require('./Router/SmsRouter'));
 app.use('/api/evc-pay/', require('./Router/EvcRouter'));
 app.use('/api/member/', require('./Router/MemRouter'));
 app.use('/api/contact/', require('./Router/ContactRouter'));
+app.use('/api/account_delete/', require('./Router/AccountDeleteRouter'));
 
 // ------------------- HEARTBEAT -------------------
 const APP_URL = process.env.APP_URL; // Set this in Render environment variables
