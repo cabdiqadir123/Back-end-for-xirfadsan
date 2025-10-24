4beb0455ee0fabdb502f0c25a5e73cef97bd962a
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-10-25 00:16:40 +0300

    backend updated

--- Router/SendRouter.js
+++ Router/SendRouter.js
@@ -42,9 +42,11 @@ sendnotify.post('/send-data', async (req, res) => {
 
 
     const message = {
+      notification: { title, body },
       data: { title, body, role: role ?? '', timestamp: uniqueId },
       android: {
         priority: 'high',
+        notification: { channel_id: 'channel_id' } // MUST match Flutter
       },
       apns: { headers: { 'apns-priority': '10' } },
       token
@@ -98,6 +100,7 @@ sendnotify.post('/send-data-to-all', async (req, res) => {
         const uniqueId = Date.now().toString();
 
         const multicastMessage = {
+          notification: { title, body },
           tokens,
           data: {
             title,
@@ -107,6 +110,7 @@ sendnotify.post('/send-data-to-all', async (req, res) => {
           },
           android: {
             priority: 'high',
+            notification: { channel_id: 'channel_id' } // MUST match Flutter
           },
           apns: { headers: { 'apns-priority': '10' } }
         };
