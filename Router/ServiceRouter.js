04973d3bcd13119d6355f955f10e4da81a3be234
cabdiqaadir123 <khadarqaalid809@gmail.com>
2025-11-01 13:01:38 +0300

    backend updated

--- Router/ServiceRouter.js
+++ Router/ServiceRouter.js
@@ -35,6 +35,7 @@ ServiceRouter.get('/allNew', (req, res) => {
     GROUP BY 
         s.service_id,
         s.name,
+        s.details,
         s.image,
         s.color,
         s.status,
@@ -145,12 +146,12 @@ ServiceRouter.post(
   '/add',
   upload.fields([{ name: 'image', maxCount: 1 }, { name: 'secondry_image', maxCount: 1 }]),
   (req, res) => {
-    const { name, color, status, created_at } = req.body;
+    const { name, details, color, status, created_at } = req.body;
     const imageBuffer1 = req.files?.image ? req.files.image[0].buffer.toString("utf-8") : null;
     const imageBuffer2 = req.files?.secondry_image ? req.files.secondry_image[0].buffer : null;
 
-    const sql = 'INSERT INTO services (name, image, secondry_image, color,status, created_at) VALUES (?, ?, ?, ?, ?,?)';
-    mysqlconnection.query(sql, [name, imageBuffer1, imageBuffer2, color, status, created_at], (error, result) => {
+    const sql = 'INSERT INTO services (name, details, image, secondry_image, color,status, created_at) VALUES (?, ?, ?, ?, ?, ?,?)';
+    mysqlconnection.query(sql, [name, details, imageBuffer1, imageBuffer2, color, status, created_at], (error, result) => {
       if (error) {
         console.error('MySQL insert error:', error);
         return res.status(500).json({ error: 'Database insert failed', details: error.message });
@@ -160,6 +161,7 @@ ServiceRouter.post(
         status: 'inserted',
         id: result.insertId, // actual auto-increment ID
         name,
+        details,
         color,
         status,
         created_at,
@@ -176,14 +178,14 @@ ServiceRouter.put(
   ]),
   (req, res) => {
     const id = req.params.id;
-    const { name, color, status } = req.body;
+    const { name, details, color, status } = req.body;
 
     const imageBuffer = req.files?.image?.[0]?.buffer.toString("utf-8");
     const secondryImageBuffer = req.files?.secondry_image?.[0]?.buffer;
 
     // Start SQL and values
-    let sql = "UPDATE services SET name = ?, color = ?, status=?";
-    const values = [name, color, status];
+    let sql = "UPDATE services SET name = ?, details=?, color = ?, status=?";
+    const values = [name, details, color, status];
 
     // Conditionally add image fields
     if (imageBuffer) {
