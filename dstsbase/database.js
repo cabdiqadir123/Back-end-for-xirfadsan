81a8140b589811c10b0cd053f8b8a2623e0d20a5
cabdiqaadir123 <khadarqaalid809@gmail.com>
2026-04-08 22:08:05 +0300

    updated commit

--- dstsbase/database.js
+++ dstsbase/database.js
@@ -1,31 +1,31 @@
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-// const mysqlconnection = mysql.createPool({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE,
-//   port: process.env.MYSQL_PORT,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
-
-// module.exports = mysqlconnection;
-
-
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const mysqlconnection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'gurihage', // change this
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = mysqlconnection;
+
+
+// const mysql = require('mysql2');
+
+// const mysqlconnection = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'gurihage', // change this
+//   port: 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = mysqlconnection;
